73e552aa60d7ad6313ab2e1a76d359a5b6eeb5f6
RuiFeng01 <chiaruifeng@gmail.com>
2024-03-01 19:12:53 +0800

    accidently deleted some stuff

--- app/src/main/java/com/example/mobilesecurity/NavGraph.kt
+++ app/src/main/java/com/example/mobilesecurity/NavGraph.kt
@@ -11,15 +11,27 @@ import com.example.mobilesecurity.model.AccountRepository
 import com.example.mobilesecurity.screens.home.HomeScreen
 import com.example.mobilesecurity.screens.home.HomeScreenViewModel
 import com.example.mobilesecurity.screens.home.HomeScreenViewModelFactory
+import com.example.mobilesecurity.screens.home.makeadmin
+import com.example.mobilesecurity.screens.home.makeadminViewModel
+import com.example.mobilesecurity.screens.home.makeadminViewModelFactory
 import com.example.mobilesecurity.screens.sign_in.SignInViewModelFactory
 import com.example.mobilesecurity.screens.sign_up.SignUpScreen
 import com.example.mobilesecurity.screens.sign_up.SignUpViewModel
 import com.example.mobilesecurity.screens.sign_up.SignUpViewModelFactory
+import com.example.mobilesecurity.screens.search.SearchScreen
+import com.example.mobilesecurity.screens.search.SearchScreenViewModel
+import com.example.mobilesecurity.screens.search.SearchScreenViewModelFactory
+
+
 
 sealed class Screen(val route: String) {
     object SignInScreen : Screen(route = "signin_screen")
     object SignUpScreen : Screen(route = "signup_screen")
     object HomeScreen: Screen(route = "home_screen")
+
+    object makeadminScreen: Screen(route= "make_admin")
+
+    object SearchScreen: Screen(route = "search_screen")
 }
 
 //ViewModel Factory
@@ -34,6 +46,15 @@ val signUpViewModelFactory = SignUpViewModelFactory(
 val homeScreenViewModelFactory = HomeScreenViewModelFactory(
     AccountRepository()
 )
+
+val searchScreenViewModelFactory = SearchScreenViewModelFactory(
+    AccountRepository()
+)
+
+val makeadminViewModelFactory = makeadminViewModelFactory(
+    AccountRepository()
+)
+
 
 
 
@@ -59,6 +80,16 @@ fun NavGraph(
         composable(Screen.HomeScreen.route) {
             val viewModel: HomeScreenViewModel = viewModel(factory = homeScreenViewModelFactory)
             HomeScreen(viewModel, navController)
+        }
+
+        composable(Screen.makeadminScreen.route) {
+            val viewModel: makeadminViewModel = viewModel(factory = makeadminViewModelFactory)
+            makeadmin(viewModel, navController)
+        }
+
+        composable(Screen.SearchScreen.route) {
+            val viewModel: SearchScreenViewModel = viewModel(factory = searchScreenViewModelFactory)
+            SearchScreen(viewModel = viewModel, navController = navController)
         }
 
     }
